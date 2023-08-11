fa4ed860e56ffc02bd34cdb0e4452b52a3020b48
toluwanibabs1 <babayanjutoluwani3@gmail.com>
2023-08-10 17:41:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return:  0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
